97f240c7a91fa38c42da4daca47d7541b35559e1
hyunnjjung <hyunnjjung@gmail.com>
2024-12-16 02:40:53 +0900

    Fix: views.py에서 redis에 저장하는 부분 수정

--- PartyLink/chat/routing.py
+++ PartyLink/chat/routing.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    path('ws/chat/<uuid:room_id>/', ChatConsumer.as_asgi()),
-]
-
+    path('ws/chat/<str:room_id>/', ChatConsumer.as_asgi()),
+]  # 대괄호가 제대로 닫혔는지 확인
